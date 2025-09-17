6dd0cdd36c5006a0ab73ea217edfb713a9878143
mehdidou1 <elkoleimehdidou@gmail.com>
2025-09-17 22:39:44 +0200

    Intégrer le framework JUnit au projet

---
+++ src/test/FirstUnitTest.java
@@ -0,0 +1,22 @@
+package test;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class FirstUnitTest {
+
+    @Test
+    public void whenThis_thenThat() {
+        assertTrue(true);
+    }
+
+    @Test
+    public void whenSomething_thenSomething() {
+        assertTrue(true);
+    }
+
+    @Test
+    public void whenSomethingElse_thenSomethingElse() {
+        assertTrue(true);
+    }
+}
